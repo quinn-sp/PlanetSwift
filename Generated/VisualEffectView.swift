bbb428ffe13f12005f66bcf34b52c88673a958b8
Nicholas Bowlin <nick.bowlin@smallplanet.com>
2015-02-05 17:56:19 -0500

    Making VisualEffectView play nice with constraints

--- Generated/VisualEffectView.swift
+++ Generated/VisualEffectView.swift
@@ -3,32 +3,42 @@
 //
 
 public class VisualEffectView: VisualEffectViewBase {
+    public var effectView: UIVisualEffectView?
+    
+    override public var view: UIView {
+        get {
+            if NSClassFromString("UIVisualEffectView") != nil {
+                if effectView != nil {
+                    return effectView!
+                }
+                
+                if effect != nil {
+                    switch effect! {
+                    case PlanetUI.BlurEffect.extraLight:
+                        effectView =  UIVisualEffectView(effect: UIBlurEffect(style: .ExtraLight))
+                    case PlanetUI.BlurEffect.light:
+                        effectView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+                    case PlanetUI.BlurEffect.dark:
+                        effectView = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
+                    }
+                }
+                if effectView != nil {
+                    return effectView!
+                }
+            }
+            return super.view
+        }
+        set {
+            if let newView = newValue as? UIVisualEffectView {
+                effectView = newView
+            } else {
+                effectView = nil
+            }
+        }
+    }
+    
     
     public override func gaxbPrepare() {
         super.gaxbPrepare()
-        
-        if effect != nil {
-            
-            if NSProcessInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 8, minorVersion: 0, patchVersion: 0)) {
-                println("iOS >= 8.0.0")
-            
-                var blur: UIVisualEffectView
-                
-                switch effect! {
-                case PlanetUI.BlurEffect.extraLight:
-                    blur = UIVisualEffectView(effect: UIBlurEffect(style: .ExtraLight))
-                case PlanetUI.BlurEffect.light:
-                    blur = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
-                case PlanetUI.BlurEffect.dark:
-                    blur = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
-                }
-                
-                blur.frame = view.bounds
-                view.addSubview(blur)
-            }
-            else {
-                println("Blur effects do not exist on iOS < 8.0.0")
-            }
-        }
     }
 }
