7b7b209ab2c45ead578b900b9167b47ad658a6e4
Brad B <brad.bambara@smallplanet.com>
2015-04-22 18:38:55 -0400

    removing a code path that is never used and doesn't work

--- PlanetSwift/Views/PlanetView.swift
+++ PlanetSwift/Views/PlanetView.swift
@@ -16,11 +16,6 @@ public class PlanetView: UIView {
 			loadXMLView()
 		}
 	}
-    
-    convenience init(bundlePath: String?) {
-        self.init()
-        self.bundlePath = bundlePath
-    }
 	
 	//MARK: - loading / unloading view
 	
