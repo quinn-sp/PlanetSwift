86bfc2f5f27dddc08267f5e1655fc2efecaefebf
Nicholas Bowlin <nick.bowlin@smallplanet.com>
2016-08-08 18:36:17 -0400

    Changing PlanetNetworkImageView's setImage url to be optional. If nil is passed than it just uses the placeholder image.

--- PlanetSwift/Views/PlanetNetworkImageView.swift
+++ PlanetSwift/Views/PlanetNetworkImageView.swift
@@ -13,12 +13,17 @@ public class PlanetNetworkImageView: UIImageView {
 	public var placeholderContentMode:UIViewContentMode = .ScaleToFill
 	public var downloadedContentMode:UIViewContentMode = .ScaleToFill
 	
-    public func setImage(url:NSURL, placeholder:UIImage? = nil, completion:((success:Bool)->Void)? = nil) {
+    public func setImage(url:NSURL?, placeholder:UIImage? = nil, completion:((success:Bool)->Void)? = nil) {
 		
 		self.contentMode = placeholderContentMode
 		self.image = placeholder
-		
-		ImageCache.sharedInstance.get(url) { [weak self] (image:UIImage?) in
+        
+        guard let imageUrl = url else {
+            completion?(success: false)
+            return
+        }
+        
+		ImageCache.sharedInstance.get(imageUrl) { [weak self] (image:UIImage?) in
 			
 			if image != nil {
 				if let downloadedContentMode = self?.downloadedContentMode {
