cc16cfa781a5a1ebec9e4f04c5fca8621e7549f7
Teju Prasad <skwadim@yahoo.com>
2018-01-18 11:41:19 -0500

    Updates to TextView to support ToolBar Buttons, and Character Counter

--- Generated/TextView.swift
+++ Generated/TextView.swift
@@ -7,8 +7,9 @@ import UIKit
 public class TextView: TextViewBase {
 
 	lazy public var textView = UITextView()
+    lazy public var textViewToolbar : UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
 	lazy private var textViewDelegate = TextViewDelegateHelper()
-	
+    
 	override public var view: UIView {
 		get {
             if textView.delegate == nil {
@@ -31,6 +32,7 @@ public class TextView: TextViewBase {
 		if let text = text {
 			textView.text = text
 		}
+        print("tv gaxb")
 		if let fontName = fontName {
             #if os(iOS)
                 textView.font = UIFont(name: fontName, size: UIFont.systemFontSize())
@@ -58,9 +60,23 @@ public class TextView: TextViewBase {
         if let tintColor = tintColor {
             textView.tintColor = tintColor
         }
+        
+        // Keyboard ToolBar Related
+        
+        if leftButtonText != nil  || rightButtonText != nil {
+            attachToolBar()
+        }
 	}
 	
-	func textViewDidChange(textView: UITextView) {
+	func textViewDidChange(_ textView: UITextView) {
+        
+        if let showMaxCount = showMaxCount, let maxCount = maxCount {
+            if showMaxCount {
+                let updatedCount : Int = Int(maxCount) - Int(textView.text.count)
+                self.updateLeftButtonText(String(updatedCount))
+            }
+        }
+        
 		if onChange != nil {
 			doNotification(onChange!)
 		}
@@ -71,8 +87,22 @@ public class TextView: TextViewBase {
 			doNotification(onEndEditing!)
 		}
 	}
+    
+    func textViewShouldChangeText(_ text: String) -> Bool {
+        
+        // Logic here to compute if there's a max count and if the text has exceeded it,
+        // do not allow any more editing.
+        
+        if let maxCount = maxCount {
+            if text.count > Int(maxCount)-1 {
+                return false
+            }
+        }
+        
+        return true
+    }
 	
-	func doNotification(note: String) {
+	func doNotification(_ note: String) {
 		let (scopeObject, name) = self.parseNotification(note)
 		if name != nil {
 			NSNotificationCenter.defaultCenter().postNotificationName(name!, object: scopeObject)
@@ -83,6 +113,111 @@ public class TextView: TextViewBase {
         text = newText
         gaxbPrepare()
     }
+    
+    // Tool Bar Related (Left/Right Button Related)
+    
+    func attachToolBar() {
+        
+        var barButtonItems : [UIBarButtonItem] = []
+    
+        textViewToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        if let leftButtonText = leftButtonText {
+            let leftBarButton: UIBarButtonItem = UIBarButtonItem(title: leftButtonText, style: .done, target: self, action:#selector(leftButtonAction))
+            
+            if showMaxCount ?? false {
+                leftBarButton.isEnabled = false
+                leftBarButton.title = String(Int(maxCount!))
+            }
+            
+            barButtonItems.append(leftBarButton)
+            barButtonItems.append(flexSpace)
+        }
+        
+        if let rightButtonText = rightButtonText {
+            let rightBarButton: UIBarButtonItem = UIBarButtonItem(title: rightButtonText, style: .done, target: self, action:#selector(rightButtonAction))
+            barButtonItems.append(rightBarButton)
+        }
+        
+        textViewToolbar.items = barButtonItems
+        textViewToolbar.sizeToFit()
+        
+        textView.inputAccessoryView = textViewToolbar
+    }
+    
+// Toolbar Action Methods
+    
+    @objc func leftButtonAction() {
+        
+        if onLeftButton != nil {
+            doNotification(onLeftButton!)
+        }
+        
+    }
+    
+    @objc func rightButtonAction() {
+        
+        if onRightButton != nil {
+            doNotification(onRightButton!)
+        }
+    }
+    
+    
+    
+// Toolbar Update Methods
+    
+    public func updateMaxCount(_ newMaxCount: Double?) {
+        maxCount = newMaxCount
+    }
+    
+    public func updateShowMaxCount(_ newShowMaxCount: Bool?) {
+        showMaxCount = newShowMaxCount
+    }
+    
+    public func updateLeftButtonText(_ newText: String?) {
+    
+        if let barButtonItems = textViewToolbar.items, let newText = newText {
+            
+            var updatedBarButtonItems : [UIBarButtonItem] = []
+            let leftBarButton: UIBarButtonItem = UIBarButtonItem(title: newText, style: .done, target: self, action:#selector(leftButtonAction))
+            
+            if showMaxCount ?? false {
+                leftBarButton.isEnabled = false
+            } else {
+                leftBarButton.isEnabled = true
+            }
+            
+            updatedBarButtonItems.append(leftBarButton)
+            updatedBarButtonItems.append(barButtonItems[1])
+            updatedBarButtonItems.append(barButtonItems[2])
+            
+            textViewToolbar.items = updatedBarButtonItems
+            textViewToolbar.sizeToFit()
+            
+            // gaxbPrepare()
+        }
+    }
+    
+    public func updateRightButtonText(_ newText: String?) {
+        
+        if let barButtonItems = textViewToolbar.items, let newText = newText {
+            
+            var updatedBarButtonItems : [UIBarButtonItem] = []
+            let rightBarButton: UIBarButtonItem = UIBarButtonItem(title: newText, style: .done, target: self, action:#selector(rightButtonAction))
+            
+            updatedBarButtonItems.append(barButtonItems[0])
+            updatedBarButtonItems.append(barButtonItems[1])
+            updatedBarButtonItems.append(rightBarButton)
+            
+            textViewToolbar.items = updatedBarButtonItems
+            textViewToolbar.sizeToFit()
+            
+            // gaxbPrepare()
+        }
+    }
+    
 }
 
 private class TextViewDelegateHelper : NSObject, UITextViewDelegate {
@@ -102,4 +237,26 @@ private class TextViewDelegateHelper : NSObject, UITextViewDelegate {
 			delegate?.textViewDidChange(textView)
 		}
 	}
+    
+    @objc func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if delegate != nil {
+            
+            if let delegate = delegate  {
+                if text.count > 0 {
+                    return delegate.textViewShouldChangeText(textView.text)
+                } else {
+                    return true
+                }
+            } else {
+                return true
+            }
+        }
+        return true
+        
+    }
+    
+
+    
+    
 }
